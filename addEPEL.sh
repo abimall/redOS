d54292e9e8eaef06ed2586de0b53450159ba965e
dfyzicka <dfyzicka@gmail.com>
2023-08-22 16:24:05 +0300

    добавляет репозитории EPEL обновляет и устанавливает нужные пакеты

--- addEPEL.sh
+++ addEPEL.sh
@@ -8,3 +8,4 @@ dnf repolist | grep epel
 sleep 3
 sudo dnf upgrade -y && sudo dnf install python3  python3-pip zip unzip neofetch speedtest-cli tldr ncdu mc curl  zsh curl exa bat ncdu cifs-utils -y
 mc
+
