4f43bed7eb090cdd719a511def87b77c6d3283e1
dfyzicka <dfyzicka@gmail.com>
2023-08-22 15:54:21 +0300

    Update addEPEL.sh

--- addEPEL.sh
+++ addEPEL.sh
@@ -5,5 +5,6 @@ sudo dnf config-manager --set-enabled crb
 sudo dnf install epel-release epel-next-release
 echo установлены EPEL репозитории
 dnf repolist | grep epel
-
+sleep 3
 sudo dnf upgrade -y && sudo dnf install python3  python3-pip zip unzip neofetch speedtest-cli tldr ncdu mc curl  zsh curl exa bat ncdu cifs-utils -y
+mc
