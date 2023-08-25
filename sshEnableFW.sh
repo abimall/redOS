21de064ede3ff6b0b8a4e00959f257e408b2b143
dfyzicka <dfyzicka@gmail.com>
2023-08-25 10:48:08 +0300

    sshd config editor

--- sshEnableFW.sh
+++ sshEnableFW.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 echo разрешить SSH
 sudo firewall-cmd --permanent --add-port=22/tcp
+sudo sed -i 's/#Port 22/Port 22/' /etc/ssh/sshd_config
+sudo sed -i 's/#PasswordAuthentication yes/PasswordAuthentication yes`/' /etc/ssh/sshd_config
