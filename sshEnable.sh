07d550de0571fbf7606d56d359b52b97137f289a
dfyzicka <dfyzicka@gmail.com>
2023-08-28 13:56:32 +0300

    ииииииииииииииииииииииииииииииииии пошел нахуй

--- sshEnable.sh
+++ sshEnable.sh
@@ -17,7 +17,7 @@ echo -e "                       ${Red}разрешить и включить SSH
 sleep 2
 sudo firewall-cmd --permanent --add-port=22/tcp
 sudo sed -i 's/#Port 22/Port 22/' /etc/ssh/sshd_config
-sudo sed -i 's/#PasswordAuthentication yes/PasswordAuthentication yes`/' /etc/ssh/sshd_config
+sudo sed -i 's/#PasswordAuthentication yes/PasswordAuthentication yes/' /etc/ssh/sshd_config
 sudo systemctl enable sshd
 sudo systemctl start sshd
 sudo systemctl status sshd
