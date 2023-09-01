538be553b1dd397d91c778a1c8ce261d6a779484
abimall <143799724+abimall@users.noreply.github.com>
2023-09-01 14:02:13 +0300

    замена стандартной нечитаемой цветовой схемы MC

--- mcskin.sh
+++ mcskin.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+
+# замена нечитаемой стандартной цветовой схемы терминального файлового менеджера MC
+# запускать от пользователя и от sudo
 mkdir -p $HOME/.local/share/mc/skins && cp -r mcskin/* $HOME/.local/share/mc/skins
                 sleep 1
 mkdir -p $HOME/.config/mc/
@@ -11,3 +14,4 @@ sed -i 's/skin=default/skin=dracula256/' $HOME/.config/mc/ini
         echo #############################################################
         sleep 3
 mc
+# авыход из mc по f10
