d43fb5b27c149b6dc0eb9f5bb134b351f7101574
abimall <143799724+abimall@users.noreply.github.com>
2023-09-01 13:55:26 +0300

    запускать после ohmyzsh2.sh

--- ohmyzsh2.sh
+++ ohmyzsh2.sh
@@ -15,23 +15,26 @@ Color_Off='\033[0m'       # Text Reset    #####
 ###############################################
 
 echo -e "${Blue}***${Red}"##"${Green}#***${Yellow}#${Blue}*#${Yellow}#***#${Yellow}#***${Purple}#**${Cyan}*#***#***#${Blue}##***#${Yellow}*#*#***${Purple}##***#***${Blue}#**${Purple}"
-
+#колонирование плагинов zsh
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k 
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting 
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-autosuggestions 
-                sleep 1
+          sleep 1
 
-
-
+#переименование файлов конфигурации в каталоге redos/confF
+echo перенос файлов конфигурации
+        sleep 2
 mv confF/ex.nanorc confF/.nanorc
         sleep 1
 mv confF/ex.zshrc confF/.zshrc
         sleep 1
 mv confF/ex.p10k.zsh  confF/.p10k.zsh
-        
+        sleep 1  
+
+#копирование файлов конфигурации в домашнюю деррикторию пользователя $HOME
 cp -R confF/.* $HOME
 sudo cp -R confF/.nanorc $HOME
-
+#создаётся алиас для neofetch
 echo 'alias neofetch="sh $HOME/neofetch.sh"' >> $HOME/.zshrc
 
 
